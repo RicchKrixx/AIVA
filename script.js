fb234e25f5d22b2d88f770a6afc9a6e89c897441
RicchKrixx <gadrijasper1@gmail.com>
2025-10-07 03:02:33 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 async function getAIResponse(message) {
-  const res = await fetch("https://aiva-backend-cp5k.onrender.com", {
+  const res = await fetch("https://aiva-backend-cp5k.onrender.com/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message })
   });
 
   const data = await res.json();
+  console.log("AIVA says:", data.choices[0].message.content);
   return data.choices[0].message.content;
 }
+
+getAIResponse("Hello AIVA!");
